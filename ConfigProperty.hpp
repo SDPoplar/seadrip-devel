2b5b906258f789f7bc94209ff665ec1395e16248
SDPoplar <741027037@qq.com>
2019-11-23 11:37:53 +0000

    some base files

---
+++ ConfigProperty.hpp
@@ -0,0 +1,96 @@
+#ifndef     __SD_CONFIG_PROPERTY_HPP__
+#define     __SD_CONFIG_PROPERTY_HPP__
+
+#include <string>
+#include <map>
+#include <fstream>
+#include <unistd.h>
+#include <seadrip/KvFileReader.hpp>
+
+namespace SeaDrip
+{
+    enum class EConfigSetFrom : int
+    {
+        DEF = 0,
+        CFGFILE =  1,
+        SHELL = 2,
+        RUNTIME = 3
+    };
+
+    template <typename T> class TConfigProperty
+    {            
+        protected:
+            SeaDrip::EConfigSetFrom m_e_from;
+            T m_o_val;
+        public:
+            TConfigProperty( T def ) : m_e_from( SeaDrip::EConfigSetFrom::DEF ), m_o_val( def ) {}
+            virtual ~TConfigProperty() {}
+            bool Set( SeaDrip::EConfigSetFrom from, T val )
+            {
+                bool canset = static_cast<int>( from ) > static_cast<int>( this->m_e_from );
+                if( !canset )
+                {
+                    return false;
+                }
+                this->m_e_from = from;
+                this->m_o_val = val;
+                return true;
+            }
+
+            T Get() const
+            {
+                return this->m_o_val;
+            }
+
+    };
+
+    class BaseConfig
+    {
+        public:
+            BaseConfig( std::string def_cfg_path ) : m_s_config_file( def_cfg_path ) {}
+            void Init( int argc, char** argv )
+            {
+                char ch;
+                while( ( ch = getopt( argc, argv, this->GetShellOptions() ) ) != -1 )
+                {
+                    if( ch == 'c' )
+                    {
+                        this->m_s_config_file.Set( EConfigSetFrom::SHELL, optarg );
+                        continue;
+                    }
+                    auto prop = this->m_map_bool_props.find( ch );
+                    if( prop != this->m_map_bool_props.end() )
+                    {
+                        prop->second->Set( EConfigSetFrom::SHELL, true );
+                    }
+                    else
+                    {
+                        this->ShellOverride( ch, optarg );
+                    }
+                }
+                KvFileReader cfg( this->m_s_config_file.Get(), '#' );
+                if( !cfg )
+                {
+                    return;
+                }
+                std::string key, val;
+                while( cfg.Next( key, val ) )
+                {
+                    auto method = this->m_map_cfgfile_override.find( key );
+                    if( method != this->m_map_cfgfile_override.end() )
+                    {
+                        method->second( val );
+                    }                
+                }
+            }
+        protected:
+            virtual char* GetShellOptions( void ) const noexcept = 0;
+            virtual void ShellOverride( char shell_flag, std::string val ) = 0;
+
+            TConfigProperty<std::string> m_s_config_file;
+            std::map<char, TConfigProperty<bool>*> m_map_bool_props;
+            std::map<std::string, void(*)( const std::string& )> m_map_cfgfile_override;
+    };
+};
+
+#endif
---
+++ KvFileReader.hpp
@@ -0,0 +1,52 @@
+#ifndef __SD_KVFILE_READER_H__
+#define __SD_KVFILE_READER_H__
+
+#include <string>
+#include <fstream>
+#include <boost/algorithm/string.hpp>
+#include <boost/regex.hpp>
+
+namespace SeaDrip
+{
+    class KvFileReader : public std::ifstream
+    {
+        public:
+            KvFileReader( std::string file_path, char note_flag ) : std::ifstream( file_path, std::ios::in ), m_c_note_flag( note_flag  ) {}
+            ~KvFileReader()
+            {
+                if( this->is_open() )
+                {
+                    this->close();
+                }
+            }
+
+            bool Next( std::string key, std::string val )
+            {
+                boost::regex pattern( "([^=]+)=\\s*([^\\s]+)" );
+                while( !this->eof() )
+                {
+                    char line[ 128 ];
+                    this->getline( line, 128 );
+                    std::string sline = boost::trim_copy( std::string( line ) );
+                    if( sline.empty() || ( sline.c_str()[ 0 ] == this->m_c_note_flag ) )
+                    {
+                        continue;
+                    }
+                    boost::smatch matches;
+                    if( !boost::regex_search( sline, matches, pattern ) || ( matches.size() < 3 ) )
+                    {
+                        continue;
+                    }
+                    key = boost::trim_copy( matches[ 1 ] );
+                    val = boost::trim_copy( matches[ 2 ] );
+                    return true;
+                }
+                return false;
+            }
+        protected:
+            char m_c_note_flag;
+    };
+};
+
+#endif
+
