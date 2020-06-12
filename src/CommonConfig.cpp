065b36687968e79ca7d401af8d876f254d7af412
SDPoplar <741027037@qq.com>
2020-06-12 07:20:43 +0000

    dev update

--- src/CommonConfig.cpp
+++ src/CommonConfig.cpp
@@ -1,4 +1,5 @@
 #include "seadrip/config/BaseConfig.h"
+#include "seadrip/shell/ShellInput.h"
 using namespace SeaDrip;
 
 //  ==================  BaseConfig  ============================================
@@ -8,5 +9,10 @@ BaseConfig::BaseConfig( std::string def_cfg_path ) : m_s_config_file( def_cfg_pa
 
 bool BaseConfig::Init( int argc, char** argv )
 {
+    auto in = ( new ShellInput() )
+        ->AppendInput( 'c', "config_file" )
+        ->AppendBool( 'd', "daemon", true );
+    for( auto item : in->Parse( argc, argv ) )
+    {}
     return false;
 }
--- src/seadrip/shell/ShellInput.h
+++ src/seadrip/shell/ShellInput.h
@@ -1,6 +1,33 @@
 #ifndef     __SD_SHELL_SHELL_INPUT_H__
 #define     __SD_SHELL_SHELL_INPUT_H__
 
+#include <string>
+#include <map>
+#include <unistd.h>
 
+namespace SeaDrip
+{
+    class ShellDictionary
+    {
+    public:
+        ShellDictionary( std::string name, std::string value );
+        bool IsBoolean() const;
+
+    private:
+        std::string name;
+        std::string value;
+    };
+
+    class ShellInput
+    {
+    public:
+        std::map<std::string, std::string> Parse( int argc, char **argv );
+        ShellInput* AppendInput( int flag, std::string alias );
+        ShellInput* AppendBool( int flag, std::string alias, bool value );
+
+    private:
+        std::map<int, ShellDictionary> items;
+    };
+};
 
 #endif
